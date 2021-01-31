519d0662052246ed6a667965d75ee1d3a1a8baf3
lanceliamll <lancelliotqqq@gmail.com>
2021-01-31 20:17:12 +0800

    Added Snowpack

---
+++ snowpack.config.js
@@ -0,0 +1,30 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    // directory name: 'build directory'
+    public: '/',
+    src: '/dist',
+  },
+  plugins: [
+    "@snowpack/plugin-sass",
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv"
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    /* Example: Bundle your final build: */
+    // "bundle": true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
