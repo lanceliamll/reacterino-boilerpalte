8942790c3a7988c4b389eeba3278353f3f8fb3ea
lanceliamll <lance.depadua@mnltechnology.com>
2021-02-02 20:15:21 +0800

    Added manifest for Install

--- public/sw.js
+++ public/sw.js
@@ -3,5 +3,16 @@ console.log("Hello PWA");
 
 //Install Service Worker
 self.addEventListener('install', ev => {
-  console.log("Success: Service Worker Installed", ev);
+  console.log("Service Worker Success: Installed", ev);
+})
+
+
+// Activate Service Worker
+self.addEventListener('activate', ev => {
+  console.log("Service Worker Success: Activated", ev);
+})
+
+// Fetch Event from Service Worker
+self.addEventListener('fetch', ev => {
+  console.log("Service Worker Success: Fetching Event", ev);
 })
